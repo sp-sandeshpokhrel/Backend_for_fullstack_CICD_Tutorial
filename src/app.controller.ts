cebc05b3ff8fdc3fd14456d42f8debfc0895dd5d
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-11-08 14:18:38 +0545

    latest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('sampledata')
+  getSampleData() {
+    return this.appService.getAllSampleData();
+  }
 }
